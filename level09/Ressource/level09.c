4a51884fc7f1243ef1f4b95ffbdd4a240d4ea93a
yalabidi <yannis.labidi666@gmail.com>
2021-04-23 11:56:40 +0200

    repo ready

---
+++ level09/Ressource/level09.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int ac, char **av) {
+
+        int i = 0;
+        while (av[1][i]) {
+                printf("%c", av[1][i] - i);
+                i++;
+        }
+        return 0;
+}
